92f29bff12bdf5bace104818bf31cc1e508371b9
jorge <jorgecsri@gmail.com>
2023-10-17 11:20:59 -0400

    segunda commit

--- src/keys.js
+++ src/keys.js
@@ -1,13 +1,21 @@
 //dir de la db
+import{
+    DB_HOST,
+ DB_USER ,
+ DB_PASSWORD ,
+ DB_NAME,
+ DB_PORT 
+} from './config.js'
+
 module.exports = {
 
     database: {
         connectionLimit: 1000,
-        host: 'localhost',//'152.70.216.169',//'localhost', //'',
-        port: '3306',
-        user: 'root',//'harold151199',//'harold',
-        password: '123456789',//'151199',
-        database: 'sw1pizarrac4'
+        host: DB_HOST,//'localhost',//'152.70.216.169',//'localhost', //'',
+        port: DB_PORT,//'3306',
+        user: DB_USER,//'root',//'harold151199',//'harold',
+        password: DB_PASSWORD,//'123456789',//'151199',
+        database: DB_NAME//'sw1pizarrac4'
         // connectionLimit: 100,
         // host: 'localhost',//'152.70.216.169',//'localhost', //'',
         // port: '3307',
